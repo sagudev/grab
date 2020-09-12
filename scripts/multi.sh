7362f7d033f69383a2739b8962c8506d202c6a17
sagudev <samo.golez@outlook.com>
2020-09-12 10:39:29 +0200

    Comments and clean

--- scripts/multi.sh
+++ scripts/multi.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# comments are nested as in GitHub MD
+# Main
+## Sub
+### sub sub
 
-## Firstly get master data
-# Build my xmltv
+# Firstly get master data
+## Build my xmltv
+### Download deps
 sudo apt install -y git libxml2-utils xmlstarlet xmltv dos2unix zip libarchive-zip-perl \
     libcgi-pm-perl \
     libdata-dump-perl \
@@ -54,40 +59,49 @@ sudo apt install -y git libxml2-utils xmlstarlet xmltv dos2unix zip libarchive-z
     make \
     perl \
     perl-tk
+### download src
 git clone https://github.com/sagudev/xmltv.git
 cd xmltv
+### build
 perl Makefile.PL --yes
 make
+### install
 sudo make install
 cd ..
 rm xmltv -rf
-echo "all" | tv_grab_si --configure
-tv_grab_si --output "epg_grab.xmltv"
-## Download data from ITAK (if failed do not merge)
-# todo merge with old
-wget -q http://freeweb.t-2.net/itak/epg_b.xml.gz || mv "epg_grab.xmltv" "epg_v2.xmltv" && exit 0
 
-# tar files are sometimes corrupted so we are using 7z to extract
-#sudo apt install p7zip-full p7zip-rar
-# tar -zxvf ./epg_b.xml.gz
+## Grab using tv_grab_si
+echo "all" | tv_grab_si --configure
+tv_grab_si --output "epg_grab.xml"
+
+# Get ITAK data
+## Download data from ITAK (if failed use old data)
+wget -q http://freeweb.t-2.net/itak/epg_b.xml.gz -O /tmp/epg_b.xml.gz && mv /tmp/epg_b.xml.gz epg_b.xml.gz || echo "Itak not working, using old"
+
+## Extract data
+### tar files are sometimes corrupted so we are using 7z to extract
+sudo apt install p7zip-full p7zip-rar -y
 7z e epg_b.xml.gz
 
-#Itak id|siol_id|siol name  empty if no
-# set the Internal Field Separator to |
+## Eq. data
+### Itak id|siol_id|siol name
+### set the Internal Field Separator to |
 IFS='|'
 while read -r itak_ch siol_id siol_name; do
-    if [ ! -z "$siol_id" ]; then # not empty, so do replace
+    if [ ! -z "$siol_id" ]; then ### not empty, so do replace
         id='id="'$itak_ch'"'
         id_r='id="'$siol_id'"'
         cn='channel="'$itak_ch'"'
         cn_r='channel="'$siol_id'"'
         n='>'$itak_ch'</display-name>'
         n_r='>'$siol_name'</display-name>'
-        # replace channel id
+        ### do replace
         sed -i -e "s,${id},${id_r},g" -e "s,${cn},${cn_r},g" -e "s,${n},${n_r},g" "epg_b.xml"
     fi
 done < ./itak.conf
-tv_sort --by-channel --output "epg_grab_s.xmltv"  "epg_grab.xmltv"
+
+# Sort and merge
+tv_sort --by-channel --output "epg_grab_s.xml"  "epg_grab.xml"
 tv_sort --by-channel --output "epg_b_s.xml"  "epg_b.xml"
-/usr/bin/tv_merge -i "epg_grab_s.xmltv" -m "epg_b_s.xml" -o "epg_v2.xmltv"
-rm epg_grab* epg_b*
+/usr/bin/tv_merge -i "epg_grab_s.xml" -m "epg_b_s.xml" -o "epg_v2.xmltv"
+rm epg_grab* epg_b*.xml
