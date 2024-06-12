5794f15238d3d660745d9f14aeb85bc0e42a8102
sagudev <16504129+sagudev@users.noreply.github.com>
2024-06-12 17:28:36 +0200

    A new start

---
+++ scripts/generate_ch_files.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+[ -d "ch" ] && rm -Rf ch
+mkdir ch
+cd ch
+# only base
+xmllint --encode utf8 --xpath '//channel' "../kanali.xml" >> "tmp.xml"
+# new lines
+sed -i 's/channel>/channel>\n/g' tmp.xml
+# do split
+split -l 38 tmp.xml -d "slo"
+# remove old
+rm tmp.xml
+# now insert start and end of file
+FILES=.
+for f in ./*
+do
+  echo "Editing file $f"
+  echo '<?xml version="1.0" encoding="UTF-8"?>
+    <settings>
+    <filename>/data/'"$f"'_wg.xml</filename>
+    <mode>m</mode>
+    <proxy>automatic</proxy>
+    <user-agent>Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; yie9)</user-agent>
+    <logging>on</logging>
+    <retry time-out="15">1</retry>
+    <timespan>7</timespan>
+  <update>i</update>' | cat - "$f" > temp && mv temp "$f.xml"
+  rm "$f"
+  echo '</settings>' >> "$f.xml"
+  xmllint "$f.xml" --format --output "$f.xml"
+done
+
+# v2
+echo "none" | tv_grab_si --configure
+sort -o tv_grab_si.conf tv_grab_si.conf
+cp  ~/.xmltv/tv_grab_si.conf set1.conf
+cp  ~/.xmltv/tv_grab_si.conf set2.conf
+cp  ~/.xmltv/tv_grab_si.conf set3.conf
+cp  ~/.xmltv/tv_grab_si.conf set4.conf
+cp  ~/.xmltv/tv_grab_si.conf set5.conf
+cp  ~/.xmltv/tv_grab_si.conf set6.conf
+cp  ~/.xmltv/tv_grab_si.conf set7.conf
+cp  ~/.xmltv/tv_grab_si.conf set8.conf
+cp  ~/.xmltv/tv_grab_si.conf set9.conf
+cp  ~/.xmltv/tv_grab_si.conf set10.conf
+sed -i -e '1,25{s/!/=/}' set1.conf
+sed -i -e '26,50{s/!/=/}' set2.conf
+sed -i -e '51,75{s/!/=/}' set3.conf
+sed -i -e '76,100{s/!/=/}' set4.conf
+sed -i -e '101,125{s/!/=/}' set5.conf
+sed -i -e '126,150{s/!/=/}' set6.conf
+sed -i -e '151,175{s/!/=/}' set7.conf
+sed -i -e '176,200{s/!/=/}' set8.conf
+sed -i -e '201,225{s/!/=/}' set9.conf
+sed -i -e '226,300{s/!/=/}' set10.conf
---
+++ scripts/merge.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+i=0
+
+for filename in wg/slo*.xml
+do
+  
+  if [[ $i -eq 0 ]]; then
+    second=$filename
+    i=$((i+1))
+    continue
+  fi
+  
+  if [[ $i -gt 1 ]]; then
+    second="epg_wg.xmltv"
+  fi
+  
+  echo -e "merge ${filename} $second"
+  tv_merge -i $filename -m $second -o "epg_wg.xmltv"
+  i=$((i+1))
+  
+done;
+
+echo "wg merge done!"
+#v2
+for filename in grab/grab_*.xml
+do
+  # Move firs one
+  if [[ "$filename" == *"grab_1.xml"* ]]; then
+    mv -f "$filename" "epg.xmltv"
+    continue
+  fi
+  echo -e "merge ${filename}"
+  tv_merge -i "${filename}" -m "epg.xmltv" -o "epg.xmltv"
+  
+done;
+
+# after merge same data is wrong
+echo "Normalize"
+xmlstarlet tr normalize.xsl epg.xmltv > epg_n.xmltv
+mv -f epg_n.xmltv epg.xmltv
